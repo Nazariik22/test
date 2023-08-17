a78170344fdfcbd673a8b015f02ab3265521e653
Admin <nazariik20@gmail.com>
2023-08-17 19:30:38 +0300

    2.0

---
+++ JS/data.js
@@ -0,0 +1,38 @@
+import { adventege, product } from "./GetState.js";
+const elementHtml = {
+    section1: document.getElementsByClassName('section1_container')[0],
+    section2: document.getElementsByClassName('section2_container')[0],
+}
+let { section1, section2 } = elementHtml
+for (let i = 0; i < adventege.length; i++) {
+    const element = document.createElement('div');
+    element.classList.add('section1_block');
+    element.innerHTML = `
+    <img src="${adventege[i].img}" alt="">
+    <h3>${adventege[i].title}</h3>
+    <p>${adventege[i].text}</p>
+    `
+    section1.insertAdjacentElement('beforeend', element)
+
+}
+for (let i = 0; i < product.length; i++) {
+    const element = document.createElement('div');
+    element.classList.add('section2_block');
+    element.innerHTML = `
+    <img src="${product[i].img}" alt="">
+    <h3>${product[i]. title}</h3>
+    <p>${product[i].text}</p>
+    <div class="flex">
+        <div class="price">
+            <b>${product[i]. price} $</b>
+            <p class="gray">${product[i].grams} гр</p>
+        </div>
+        <button class="button_gradient">
+        Заказать
+        <img src="img/main/cost/6.svg" alt="">
+        </button>
+    `
+    section2.insertAdjacentElement('beforeend', element)
+
+}
+
